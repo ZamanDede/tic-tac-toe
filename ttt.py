332463d2f8e5a631865a05f301ba3fb6655f4bd5
ZamanDede <yamantosunoglu.1@gmail.com>
2021-09-26 19:05:02 +0300

    the first function of game added

--- ttt.py
+++ ttt.py
@@ -1,12 +1,19 @@
 
 finished = True
 
+table = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
 def print_table():
-    table = "_ _ _
-    _ _ _
-    _ _ _"
+    print("\n")
+    print(table[0] + " | " + table[1] + " | " + table[2] + "     1 | 2 | 3")
+    print(table[3] + " | " + table[4] + " | " + table[5] + "     4 | 5 | 6")
+    print(table[6] + " | " + table[7] + " | " + table[8] + "     7 | 8 | 9")
+    print("\n")
+    
 def update_table(pos,mark):
-    pass
+    table[pos-1] = mark
 def check_result():
     pass
 
@@ -14,12 +21,12 @@ def check_result():
 def play_game():
     while finished:
         print_table()
-        player1p = input("player 1 choose a position 1-9: ")
+        player1p = int(input("player 1 choose a position 1-9: "))
         player1m = input("player 1 choose a mark x-o: ")
         update_table(player1p,player1m)
         print_table()
         check_result()
-        player2p = input("player 2 choose a position 1-9: ")
+        player2p = int(input("player 2 choose a position 1-9: "))
         player2m = input("player 2 choose a mark x-o: ")
         update_table(player2p,player2m)
         print_table()
