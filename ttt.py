2b6090d67afde2744eff32bc0e07cba450005009
ZamanDede <yamantosunoglu.1@gmail.com>
2021-09-26 19:41:20 +0300

    Game is running, next we will add the result property.

--- ttt.py
+++ ttt.py
@@ -12,33 +12,59 @@ def print_table():
     print(table[6] + " | " + table[7] + " | " + table[8] + "     7 | 8 | 9")
     print("\n")
     
-def update_table(pos,mark):
-    table[pos-1] = mark
+def update_table(pos,mark,player):
+    if table[pos-1] == "-":
+        table[pos-1] = mark
+    else:
+        print("The position is full!")
+        if player == 1:
+            player1()
+        else:
+            player2()
+    
 def check_result():
     pass
 
-
+def player1():
+    print_table()
+    while True:
+        player1p = int(input("player 1 choose a position 1-9: "))
+        if 0<player1p<10:
+            player1m = input("player 1 choose a mark x-o: ")
+            if player1m == "x" or player1m == "o":
+                break
+            else:
+                print("Invalid mark try again!")
+        else:
+            print("Invalid position try again!")
+            
+    update_table(player1p,player1m,1)
+    print_table()
+    check_result()
+        
+def player2():
+    while True:
+        player2p = int(input("player 2 choose a position 1-9: "))
+        if 0<player2p<10:
+            player2m = input("player 2 choose a mark x-o: ")
+            if player2m == "x" or player2m == "o":
+                break
+            else:
+                print("Invalid mark try again!")
+        else:
+            print("Invalid position try again!")
+    update_table(player2p,player2m,2)
+    print_table()
+    check_result()
 def play_game():
     while finished:
-        print_table()
-        player1p = int(input("player 1 choose a position 1-9: "))
-        player1m = input("player 1 choose a mark x-o: ")
-        update_table(player1p,player1m)
-        print_table()
-        check_result()
-        player2p = int(input("player 2 choose a position 1-9: "))
-        player2m = input("player 2 choose a mark x-o: ")
-        update_table(player2p,player2m)
-        print_table()
-        check_result()
+        player1()
+        player2()
+        if not finished:
+            break
+        
 
 
 
 play_game()
 
-'''
-print("_ _ _")
-print("_ _ _")
-print("_ _ _")
-
-'''
